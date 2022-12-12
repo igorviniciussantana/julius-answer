a028b30ceabb5a00d05fd3e557900db874223fb7
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-12-12 10:02:34 -0400

    style: auto-format using prettier

--- components/lista.tsx
+++ components/lista.tsx
@@ -1,56 +1,47 @@
 import styles from "../styles/lista/lista.module.css";
 import ListaItem from "./listaItem";
 
-interface ListaProps{
-mensagem: string;
+interface ListaProps {
+  mensagem: string;
 }
 
-
-
-export default function Lista(props : ListaProps) {
+export default function Lista(props: ListaProps) {
   const alfabeto = "abcdefghijklmnopqrstuvwxyz";
   var counter = 1;
 
   var alfabetoArray = alfabeto.split("");
   var mensagemArray = props.mensagem.split("");
 
-
   return (
     <div className={styles.lista}>
       <h1 className={styles.title}>Todas as possibilidades</h1>
       {/* Executa uma função para letra do alfabeto */}
       {alfabetoArray.map((letra, index) => {
-
-        var saida : string[] = [];
+        var saida: string[] = [];
 
         return (
           <div key={index} className={styles.itens}>
             <>
-            {mensagemArray.map((letter) => {
-              var retorna = alfabetoArray.indexOf(letter);
-              var indice = retorna + counter;
-              if (letter != ' ') {
-                if (indice < 26) {
-                  saida.push(alfabetoArray[indice]);
+              {mensagemArray.map((letter) => {
+                var retorna = alfabetoArray.indexOf(letter);
+                var indice = retorna + counter;
+                if (letter != " ") {
+                  if (indice < 26) {
+                    saida.push(alfabetoArray[indice]);
+                  } else {
+                    saida.push(alfabetoArray[indice - 26]);
+                  }
                 } else {
-                  saida.push(alfabetoArray[indice - 26]);
+                  saida.push(" ");
                 }
-              } else {
+              })}
 
-                saida.push(' ');
-
-              }
-            })}
-
-            <ListaItem id={index} saida={saida} />
-            <span className={styles.none} >{counter++}</span>
+              <ListaItem id={index} saida={saida} />
+              <span className={styles.none}>{counter++}</span>
             </>
           </div>
-        )
-      }
-
-      )}
+        );
+      })}
     </div>
   );
-
 }
